91f2c0ef07e483aa34285e51ca947bce4a8c5ccf
samsonjisso <samsonjisso300@gmail.com>
2026-02-09 23:59:27 +0300

    auth google function fix

---
+++ features/auth/src/commonMain/kotlin/dev/shewanevibs/auth/components/GoogleFonts.kt
@@ -0,0 +1,11 @@
+package dev.shewanevibs.auth.components
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GoogleFonts() {
+    Button(onClick = { /*TODO*/ }) {
+
+    }
+}
